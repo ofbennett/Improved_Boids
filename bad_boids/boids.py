36069a9e47ff5ad0454f9aa3c7268c4ffdd45cea
ofbennett <ofbennett@gmail.com>
2015-12-14 19:23:25 +0000

    Replaced magic number with copycat_radius variable

--- bad_boids/boids.py
+++ bad_boids/boids.py
@@ -11,6 +11,7 @@ import random
 
 middle_attraction = 0.01
 avoidance_radius = 10
+copycat_radius = 100
 
 boids_x=[random.uniform(-450,50.0) for x in range(50)]
 boids_y=[random.uniform(300.0,600.0) for x in range(50)]
@@ -36,7 +37,7 @@ def update_boids(boids):
 	# Try to match speed with nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
+			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < copycat_radius**2:
 				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
 				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
 	# Move according to velocities
