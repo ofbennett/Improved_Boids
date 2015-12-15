7a9d81691f0f181a10ac9babd6fd91b884f9f3c0
ofbennett <ofbennett@gmail.com>
2015-12-15 15:05:01 +0000

    Old bad-boids code removed along with its regression test

--- bad_boids/boids.py
+++ bad_boids/boids.py
@@ -14,12 +14,6 @@ copycat_radius = 100.0
 copycat_influence = 0.125
 boid_num = 50
 
-boids_x=[random.uniform(-450,50.0) for x in range(boid_num)]
-boids_y=[random.uniform(300.0,600.0) for x in range(boid_num)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(boid_num)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(boid_num)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
-
 class Boid(object):
 	def __init__(self,x,y,vx,vy):
 		self.position = np.array([x,y])
@@ -84,46 +78,14 @@ swarm = Swarm()
 swarm.hatch(boid_num)
 boid_xs,boid_ys = swarm.boidPositions()
 
-def update_boids(boids):
-	xs,ys,xvs,yvs=boids
-	# Fly towards the middle
-	for i in range(boid_num):
-		for j in range(boid_num):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*middle_attraction/boid_num
-	for i in range(boid_num):
-		for j in range(boid_num):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*middle_attraction/boid_num
-	# Fly away from nearby boids
-	for i in range(boid_num):
-		for j in range(boid_num):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < avoidance_radius**2:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
-	# Try to match speed with nearby boids
-	for i in range(boid_num):
-		for j in range(boid_num):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < copycat_radius**2:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*copycat_influence/boid_num
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*copycat_influence/boid_num
-	# Move according to velocities
-	for i in range(boid_num):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
-
-
 figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-# scatter=axes.scatter(boids[0],boids[1])
 scatter=axes.scatter(boid_xs,boid_ys)
 
 def animate(frame):
 	swarm.update()
 	xs,ys = swarm.boidPositions()
 	scatter.set_offsets(zip(xs,ys))
-
- #   	update_boids(boids)
- #   	scatter.set_offsets(zip(boids[0],boids[1]))
-
 
 anim = animation.FuncAnimation(figure, animate,
                                frames=50, interval=50)
--- bad_boids/tests/test_boids.py
+++ bad_boids/tests/test_boids.py
@@ -1,15 +1,7 @@
-from bad_boids.boids import update_boids, Boid, Swarm
+from bad_boids.boids import Boid, Swarm
 from nose.tools import assert_almost_equal, assert_equal, assert_less, assert_greater
 import os
 import yaml
-
-def test_bad_boids_regression():
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixtures','fixture.yml')))
-    boid_data=regression_data["before"]
-    update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
-        for after_value,before_value in zip(after,before):
-            assert_almost_equal(after_value,before_value,delta=0.01)
 
 def test_OO_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixtures','fixture.yml')))
