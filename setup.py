138187d8bcd40c62a6a5f898e2641b6d986b3ea7
ofbennett <ofbennett@gmail.com>
2016-02-03 18:27:14 +0000

    Meta-data added to setup file. .py extension dropped from entry point file

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'Improved_Boids',
-    version = '1.0',
+    version = '1.0.0',
+    author = 'Oscar Bennett',
+    licence = 'The MIT License',
+    description = 'An improved refactored version of the BadBoids code',
     packages = find_packages(exclude = ['*test']),
-    scripts = ['scripts/runBoids.py'],
+    scripts = ['scripts/runBoids'],
     install_requires = ['numpy','matplotlib','pyyaml']
 )
